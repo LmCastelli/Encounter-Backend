7003adeb7e855b2cd812e62c07087ae61bcac7d0
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2022-11-16 10:56:34 -0600

    Fixed naming error on seed files

--- api/data/seeds/04-starting-encounters.js
+++ api/data/seeds/04-starting-encounters.js
@@ -2,7 +2,7 @@ exports.seed = function(knex) {
     return knex('encounters')
         .then(function () {
             return knex('encounters').insert([
-                {name: "The first fight!", description: "The players encounter a small pack of wolves"}, {name: "The first BBEG encounter!", description: "The players meet the bbeg who attacks with an aoe spell before leaving his minions to finish the townsfolk"}
+                {name: "The first fight!", details: "The players encounter a small pack of wolves"}, {name: "The first BBEG encounter!", details: "The players meet the bbeg who attacks with an aoe spell before leaving his minions to finish the townsfolk"}
             ])
         })
 }
